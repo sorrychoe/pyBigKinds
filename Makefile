182a766fe875a98033726f7c6f0c94ee3561d68a
sorrychoe <jaeseong.choe@makinarocks.ai>
2023-04-18 10:56:41 +0900

    add typing & docstring

---
+++ Makefile
@@ -0,0 +1,58 @@
+.PHONY: init install uninstall wheel format lint test clean
+
+NAME = BigkindsParser
+
+SHELL := bash
+python = python3
+ifeq ($(OS),Windows_NT)
+	python := python
+endif
+
+ifdef user
+	pip_user_option = --user
+endif
+
+init_environment = $(python) -m pip install $(pip_user_option) --upgrade pip && \
+$(python) -m pip install $(pip_user_option) --upgrade 'build>=0.7' 'setuptools>=61.0,<64.0' 'wheel>=0.37' && \
+$(python) -m pip install $(pip_user_option) -r requirements.txt
+
+build_wheel = $(python) setup.py bdist_wheel
+
+uninstall_extension = $(python) -m pip uninstall --yes $(NAME)
+
+ifndef production
+	init_environment += git-lfs install && \
+	$(python) -m pre_commit install
+
+	install_extension = $(python) -m pip install $(pip_user_option) -e .
+else
+	install_extension = $(build_wheel) && \
+	$(python) -m pip install $(pip_user_option) --find-links "dist/" $(NAME)
+endif
+
+init:
+	$(init_environment)
+
+install:
+	$(install_extension)
+
+uninstall:
+	$(uninstall_extension)
+
+wheel:
+	$(build_wheel)
+
+format:
+	$(python) -m black --config=pyproject.toml BigKindsParser/ setup.py
+	$(python) -m isort --settings-file=.isort.cfg BigKindsParser/ setup.py
+
+lint:
+	$(python) -m flake8 --config=.flake8 BigKindsParser/ setup.py
+	$(python) -m mypy --config-file=.mypy.ini BigkindsParser/ setup.py
+	$(python) -W ignore::DeprecationWarning -m pylint --rcfile=.pylintrc BigkindsParser/ setup.py
+
+clean:
+	shopt -s globstar ; \
+	rm -fr BigKindsParser.egg-info/ ; \
+	rm -fr build/ dist/ ; \
+	rm -fr **/.mypy_cache **/__pycache__ ;
--- bigkindsparser/BigKindsParser.py
+++ bigkindsparser/BigKindsParser.py
@@ -1,55 +1,88 @@
-def press_counter(f): #언론사 별 보도 빈도 
-    freq = df['언론사'].value_counts() 
-    brod_df = pd.DataFrame(freq).reset_index() 
-    brod_df.rename(columns = {'index':'언론사', '언론사':'기사'}, inplace = True) 
+from typing import Any, Dict, List
+
+import matplotlib as plt
+import pandas as pd
+import wordcloud
+
+
+def press_counter(df: Any) -> Any:
+    """
+    언론사 별 보도 빈도
+    """
+    freq = df["언론사"].value_counts()
+    brod_df = pd.DataFrame(freq).reset_index()
+    brod_df.rename(columns={"index": "언론사", "언론사": "기사"}, inplace=True)
     return brod_df
 
-def keywords_list(df): #키워드를 list로 변환
-    return df['키워드'].values.tolist()
 
-def keyword_parser(text_list): #키워드 파싱
-    news_key = []
+def keywords_list(df: Any) -> List[str]:
+    """
+    키워드를 list로 변환
+    """
+
+    return df["키워드"].values.tolist()
+
+
+def keyword_parser(text_list: List[str]) -> List[str]:
+    """
+    키워드 파싱
+    """
+    news_key: List[str] = []
     for word in text_list:
-        word = word.split(',') #',' 기점으로 스트링 분리
-        news_key.append(word) #분리된 단어들을 리스트에 새로 정렬
+        word = word.split(",")  # ',' 기점으로 스트링 분리
+        news_key.append(word)  # 분리된 단어들을 리스트에 새로 정렬
     return news_key
 
-def duplication_remover(news_key): #중복 값 제거
-    news_value = []
+
+def duplication_remover(news_key: List[str]) -> List[str]:
+    """
+    중복 값 제거
+    """
+    news_value: List[str] = []
     for j in news_key:
-      j = list(set(j)) #중복 제거
-      news_value.append(j) #제거된 리스트를 새로운 리스트에 삽입
+        j = list(set(j))  # 중복 제거
+        news_value.append(j)  # 제거된 리스트를 새로운 리스트에 삽입
     return news_value
 
-def word_counter(news_value): #단어 갯수 카운트
-    key_words = {}
+
+def word_counter(news_value: List[str]) -> Dict[str, str]:
+    """
+    단어 갯수 카운트
+    """
+    key_words: Dict = {}
     for k in range(len(news_value)):
-          for i in news_value[k]:
-              if i not in key_words:
-                  key_words[i] = 1 #최초 언어
-              elif i in key_words:
-                  key_words[i] += 1 #중복 언어
+        for i in news_value[k]:
+            if i not in key_words:
+                key_words[i] = 1  # 최초 언어
+            elif i in key_words:
+                key_words[i] += 1  # 중복 언어
     return key_words
 
-def counter_to_DataFrame(key_words): #counter dict --> dataframe
+
+def counter_to_DataFrame(key_words: Dict) -> Any:
+    """
+    counter dict --> dataframe
+    """
     word_df = pd.DataFrame(key_words.items())
-    word_df.columns = ['단어', '빈도']
-    word_df = word_df.sort_values(['빈도'],ascending = False).reset_index(drop = True) #내림차순 정렬
+    word_df.columns = ["단어", "빈도"]
+    word_df = word_df.sort_values(["빈도"], ascending=False).reset_index(drop=True)  # 내림차순 정렬
     return word_df
 
-def press_keywords_wordcloud(df, press): #pipeline set
-    df_keywords = df[df['언론사'].str.contains(press)]['키워드'] #언론사 별 키워드를바로 워드클라우드로 전환
+
+def press_keywords_wordcloud(df: Any, press: str) -> None:
+    """
+    make keyword wordcloud
+    """
+    df_keywords = df[df["언론사"].str.contains(press)]["키워드"]  # 언론사 별 키워드를바로 워드클라우드로 전환
     keywords = keywords_list(df_keywords)
     news_key = keyword_parser(keywords)
     news_key = duplication_remover(news_key)
     key = word_counter(news_key)
     news_key = counter_to_DataFrame(key)
-    wc = WordCloud(font_path = './NanumBarunGothic.ttf', #본인이 선호하는 폰트 설정
-                    width = 500,
-                    height = 500,
-                    background_color='white').generate_from_frequencies(news_key.set_index('단어').to_dict()["빈도"])
+    wc = wordcloud.WordCloud(
+        font_path="font/NanumGothic.ttf", width=500, height=500, background_color="white"
+    ).generate_from_frequencies(news_key.set_index("단어").to_dict()["빈도"])
 
     plt.imshow(wc)
-    plt.axis('off')
-    plt.show() # 워드클라우드 출력
-    
+    plt.axis("off")
+    plt.show()  # 워드클라우드 출력
